831e7c82b3443cd124e2377f1308efa157b0c46b
Citt2024 <mail@mail.com>
2024-05-23 16:09:36 -0400

    agregado input de usuario

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let app = document.getElementById("app");
 
 let message = document.createElement("h1");
-message.innerText = "Hola, mundo!";
+
+let usr_msg = prompt("Dime tu nombre: ");
+
+message.innerText = `Hola, ${usr_msg}!`;
 
 app.appendChild(message);
